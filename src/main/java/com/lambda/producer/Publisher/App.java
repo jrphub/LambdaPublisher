a74e921c6c7e2d00b5e554d3188c486030f60446
jrphub <jyotiranjanpattnaik@gmail.com>
2018-04-18 09:22:55 +0530

    initial commit

---
+++ src/main/java/com/lambda/producer/Publisher/App.java
@@ -0,0 +1,90 @@
+package com.lambda.producer.Publisher;
+
+import java.util.Arrays;
+import java.util.Properties;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import com.twitter.hbc.ClientBuilder;
+import com.twitter.hbc.core.Client;
+import com.twitter.hbc.core.Constants;
+import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
+import com.twitter.hbc.core.processor.StringDelimitedProcessor;
+import com.twitter.hbc.httpclient.auth.Authentication;
+import com.twitter.hbc.httpclient.auth.OAuth1;
+
+/**
+ * Kafka Publisher
+ *
+ */
+public class App {
+	private final static String TOPIC = "lambda-demo";
+	private final static String BROKERS = "localhost:9092";
+
+	private static KafkaProducer<String, String> createProducer() {
+		Properties props = new Properties();
+		props.put("bootstrap.servers", BROKERS);
+		props.put("client.id", "LambdaProducer");
+		props.put("key.serializer",
+				"org.apache.kafka.common.serialization.StringSerializer");
+		props.put("value.serializer",
+				"org.apache.kafka.common.serialization.StringSerializer");
+		return new KafkaProducer<String, String>(props);
+	}
+
+	private static Authentication authorize() {
+		String consumerKey = "EcL6OsfBE9FrFaW6KcH3uUQ8J";
+		String consumerSecret = "ZWNKFBDAKay4DXqdoIUafhiKGQUoa2kdmWI1NH8nSgwCcTYOke";
+		String token = "104394125-YiXvzWIBnufh3OF99XfG3KAY9MmbOiBNGHCSM913";
+		String secret = "U10bCov5D5z70R2oaRdiWsteLXaS3HQGTW6Yx3e5jUt15";
+		Authentication auth = new OAuth1(consumerKey, consumerSecret, token,
+				secret);
+		return auth;
+	}
+
+	static void runProducer(final String term) throws Exception {
+		final KafkaProducer<String, String> producer = createProducer();
+
+		BlockingQueue<String> queue = new LinkedBlockingQueue<String>(10000);
+		StatusesFilterEndpoint endpoint = new StatusesFilterEndpoint();
+		// add some track terms
+		String[] terms = term.split(",");
+		endpoint.trackTerms(Arrays.asList(terms));
+
+		// Authorize
+		Authentication auth = authorize();
+
+		// Create a new BasicClient. By default gzip is enabled.
+		Client client = new ClientBuilder().hosts(Constants.STREAM_HOST)
+				.endpoint(endpoint).authentication(auth)
+				.processor(new StringDelimitedProcessor(queue)).build();
+
+		// Establish a connection
+		client.connect();
+		try {
+			for (int msg=0; msg <= 1000 ; msg++) {
+				ProducerRecord<String, String> record = new ProducerRecord<String, String>(
+						TOPIC, Integer.toString(msg), queue.take());
+				producer.send(record);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			producer.close();
+		}
+		
+		client.stop();
+	}
+
+	public static void main(String... args) throws Exception {
+		if (args.length == 0) {
+			runProducer("#trending");
+		} else {
+			runProducer(args[0]);
+		}
+	}
+
+}
